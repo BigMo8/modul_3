7853b19f482f7cd92bcf355ff63a1e2c0b5a4488
Monika Birgiel Marszal <monikabirgiel@gmail.com>
2023-02-25 15:40:33 +0100

    Init project

---
+++ lista_zakupow.py
@@ -0,0 +1,21 @@
+lista_zakupow = {
+    "warzywniak": ("marchew", "groch"),
+    "piekarnia" : ("bułka", "bibułka"),
+    "kiosk":("gazeta")
+}
+print (lista_zakupow)
+
+for shops,products in lista_zakupow.items():
+    print ("Idę do", shops, "i kupuję tam:", products)
+print ("łączanie kupuję tam:", len(products), "rzeczy")
+
+numbers_chosen = ([])
+for numbers in range (1,100):
+    if numbers % 5 == 0:
+            numbers_chosen.append(numbers)
+print (numbers_chosen)
+
+cubes=[]
+for numbers in numbers_chosen:
+    cubes.append(numbers*numbers*numbers)
+print(cubes)
